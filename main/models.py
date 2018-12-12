4224ebfcd24a8300c41f58d9f78fb18b01a763c7
stanley kimani <stanleykimani@stanleys-MacBook-Air.local>
2018-12-12 15:52:09 -0500

    added models

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    image = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    summary = models.CharField(max_length=1000)
+
+class Article(models.Model):
+    image = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    content = models.CharField(max_length=3000)
+
+class Comment(models.Model):
+    comments = models.CharField(max_length=255)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'main'
 ]
 
 MIDDLEWARE = [
